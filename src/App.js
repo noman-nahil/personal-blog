de6e468fe3681d77eba7340eea14bab5c1c92755
Abdullah A. Noman <43316038+noman-nahil@users.noreply.github.com>
2021-10-01 11:38:02 +0600

    adding not found page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 //from components
 import Home from './components/Home';
 import About from './components/About';
 import Article from './components/Article';
 import ArticleList from './components/ArticleList';
-
+import NotFound from './components/NotFound'
 import Navbar from './Navigation/Navbar';
 
 
@@ -13,10 +13,13 @@ function App() {
     <div>
       <Navbar />
       <div className="max-w-screen-md mx-auto pt-20">
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/article/:name" component={Article} />
-        <Route exact path="/article-list" component={ArticleList} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/article/:name" component={Article} />
+          <Route exact path="/article-list" component={ArticleList} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
     </div>
   );
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Articles from './Articles'
 import ArticlesContent from './Articles-contents'
-
+import NotFound from './NotFound'
 
 const Article = ({ match }) => {
     const name = match.params.name
     const article = ArticlesContent.find((article) => article.name === name)
-    if (!article) return <h1>Article Not Found</h1>
+    if (!article) return <NotFound />
     const otherArticles = ArticlesContent.filter(article => article.name !== name)
     const otherart = otherArticles.map((x, index) => {
         return (
